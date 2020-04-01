238b84de7b0b8ffb2ab5ae1d5c30295e7b04f2cc
Daniel Wakefield <dan@danielwakefield.co.uk>
2020-04-01 06:08:15 +0100

    First Commit

---
+++ components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="header-state" class="block" :class="{'active': menuClose, 'inactive': !menuClose}">
+    <header class="fixed z-50 flex bg-black w-full mx-auto my-0 px-10 py-3" :class="{'active': menuClose, 'inactive': !menuClose}">
+      <div class="flex-grow text-gray-700 w-0">
+        <Icons />
+      </div>
+      <div class="flex-none w-8">
+        <Logo />
+      </div>
+      <div class="flex flex-grow justify-end items-center w-0">
+        <Nav />
+        <Hamburger :menu-close="menuClose" @toggle="menuClose = !menuClose" />
+      </div>
+    </header>
+    <Menu />
+  </div>
+</template>
+
+<script>
+import Icons from '~/components/Icons.vue'
+import Logo from '~/components/Logo.vue'
+import Nav from '~/components/Nav.vue'
+import Hamburger from '~/components/Hamburger.vue'
+import Menu from '~/components/Menu.vue'
+export default {
+  components: {
+    Icons,
+    Logo,
+    Nav,
+    Hamburger,
+    Menu
+  },
+  props: {
+    menuClose: Boolean
+  }
+}
+</script>
